fixing egomotion normalization function to normalize all samples for an arbitrary batch size

Model/model_components/auto_e2e.py
@@ -37,15 +37,16 @@ class AutoE2E(nn.Module):
         # Normalization of the egomotion history vector
       
         def normalize_egomotion(egomotion_history):
-            for i in range(0, len(egomotion_history[0]), 4):
-                # speed normalized equals raw speed in m/s divided by 33
-                # corresponding to a max speed of 74 mph
-                egomotion_history[0][i] = egomotion_history[0][i]/33
-      
-                # acceleration normalized equals raw acceleration in 
-                # ms/2 divided by 8, since that equates to harsh
-                # emergency braking maneouvre
-                egomotion_history[0][i+1] = egomotion_history[0][i+1]/8
+            for b in range(0, len(egomotion_history)):
+                for i in range(0, len(egomotion_history[b]), 4):
+                    # speed normalized equals raw speed in m/s divided by 33
+                    # corresponding to a max speed of 74 mph
+                    egomotion_history[b][i] = egomotion_history[b][i]/33
+        
+                    # acceleration normalized equals raw acceleration in 
+                    # ms/2 divided by 8, since that equates to harsh
+                    # emergency braking maneouvre
+                    egomotion_history[b][i+1] = egomotion_history[b][i+1]/8
 
         self.normalize_egomotion = normalize_egomotion
 